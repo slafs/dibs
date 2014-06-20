603fa5cd3891ffb14727d537e9ce8b863d717b33
Sławek Ehlert <slafs@op.pl>
2014-06-20 23:38:28 +0200

    naive but working first basic implementation

--- src/dibsangular/static/dibsangular/js/controllers.js
+++ src/dibsangular/static/dibsangular/js/controllers.js
@@ -2,23 +2,52 @@
 /* Controllers */
 
 angular.module('dibsApp.controllers', []).
-  controller('ListCtrl', function($scope, $http) {
+  controller('ListCtrl', function($scope, $http, $log) {
     'use strict';
+
+    $scope.items = [];
+
+    /* sets a given item in scope */
+    var set_item = function(item) {
+      $log.debug("set_item");
+      $log.debug(item);
+      for (var i = 0; i < $scope.items.length; i++) {
+        $log.debug($scope.items[i]);
+        if ($scope.items[i].id === item.id) {
+          $log.debug("found");
+          $scope.items[i] = item;
+          break;
+        }
+      }
+    };
 
     $http.get('/api/v1/items/').success(function(data) {
       $scope.items = data;
     });
 
+  // }).controller
+
+    $scope.refresh = function(item) {
+      $http.get(item.url).success(function(data){
+        var item = data;
+        set_item(item);
+      });
+    };
+
     $scope.lock = function (item) {
         $http.post(item.url + 'lock/').success(function(data) {
-          console.log("post OK", data);
+          $log.debug(data);
+          $scope.refresh(item);
         });
-        console.log("locking", item);
-        item.locked_by = 'mua';
+    };
+    $scope.unlock = function (item) {
+        $http.post(item.url + 'unlock/').success(function(data) {
+          $log.debug(data);
+          $scope.refresh(item);
+        });
     };
 
   })
   .controller('MyCtrl2', [function() {
 
   }]);
-
