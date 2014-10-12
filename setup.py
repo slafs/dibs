f49c6e55ec503c168158bb43ee8db6a67eb64650
Sławek Ehlert <slafs@op.pl>
2014-10-12 18:32:17 +0200

    don't claim python3 compatibilty
    since Twisted doesn't support python3 yet
    but test the codebase against python3

--- setup.py
+++ setup.py
@@ -59,9 +59,9 @@ setup(
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.3',
+        #'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
 )
