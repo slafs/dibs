f82df367791a5e2d37f311409ef78ccebba1f3b2
Sławek Ehlert <slafs@op.pl>
2014-10-12 14:51:28 +0200

    default redirection

--- src/dibs/urls.py
+++ src/dibs/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
 from dibs.api import ItemViewSet
-# from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 # from django.conf import settings
 # from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -28,3 +28,8 @@ urlpatterns = \
 urlpatterns += staticfiles_urlpatterns()
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += \
+    patterns('',
+             url(r'^$', RedirectView.as_view(permanent=False, pattern_name = 'index')),
+             )
